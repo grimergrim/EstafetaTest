7efc6887c9ea9af4f1806f6437c3a06c2264c4c2
grimergrim <iavorskyi.v.v@gmail.com>
2016-12-01 20:33:19 +0200

    Add. Searching.

--- app/src/main/java/com/avtologistika/test/screens/main/MainActivity.java
+++ app/src/main/java/com/avtologistika/test/screens/main/MainActivity.java
@@ -10,13 +10,19 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.avtologistika.test.GlobalApplication;
 import com.avtologistika.test.R;
 import com.avtologistika.test.api.Constants;
+import com.avtologistika.test.entities.Task;
 import com.avtologistika.test.screens.main.adapters.TaskAdapter;
+import com.avtologistika.test.screens.search.SearchResultsActivity;
 import com.avtologistika.test.screens.task.TaskActivity;
 import com.avtologistika.test.utils.InMemoryCache;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -49,14 +55,72 @@ public class MainActivity extends AppCompatActivity implements MainContract.Main
         startActivity(intent);
     }
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.options_menu, menu);
+//        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+//        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+//        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+//        return true;
+//    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        return true;
+
+        SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                // this is your adapter that will be filtered
+//                adapter.getFilter().filter(newText);
+                return true;
+            }
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                // this is your adapter that will be filtered
+//                adapter.getFilter().filter(query);
+                List<Task> searchResults = mInMemoryCache.search(query);
+                if (null != searchResults && searchResults.size() > 0) {
+                    Intent intent = new Intent(getApplicationContext(), SearchResultsActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), getResources().
+                            getString(R.string.no_results), Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        };
+        searchView.setOnQueryTextListener(queryTextListener);
+
+//        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+//        searchView.setOnQueryTextListener(this);
+//
+//        menuItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+//            @Override
+//            public boolean onMenuItemActionExpand(MenuItem item) {
+//                return true;
+//            }
+//
+//            @Override
+//            public boolean onMenuItemActionCollapse(MenuItem item) {
+//                // Do something when collapsed
+//                getLoaderManager().initLoader(1, null, SearchResultsActivity.this);
+//                return true; // Return true to collapse action view
+//            }
+//        });
+
+        searchView.setIconifiedByDefault(true);
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
--- app/src/main/java/com/avtologistika/test/utils/InMemoryCache.java
+++ app/src/main/java/com/avtologistika/test/utils/InMemoryCache.java
@@ -2,11 +2,14 @@ package com.avtologistika.test.utils;
 
 import com.avtologistika.test.entities.Task;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryCache {
 
     private List<Task> mTaskList;
+
+    private List<Task> mSearchResultTaskList;
 
     public List<Task> getTaskList() {
         return mTaskList;
@@ -14,5 +17,49 @@ public class InMemoryCache {
 
     public void setTaskList(List<Task> taskList) {
         mTaskList = taskList;
+    }
+
+    public List<Task> getSearchResultTaskList() {
+        return mSearchResultTaskList;
+    }
+
+    public void setSearchResultTaskList(List<Task> searchResultTaskList) {
+        mSearchResultTaskList = searchResultTaskList;
+    }
+
+    public List<Task> search(String query) {
+        List<Task> results = new ArrayList<>();
+
+        for (Task task : mTaskList) {
+            if (null != task.getNumber()
+                    && task.getNumber().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getPlannedStartDate()
+                    && task.getPlannedStartDate().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getPlannedEndDate()
+                    && task.getPlannedEndDate().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getActualStartDate()
+                    && task.getActualStartDate().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getActualEndDate()
+                    && task.getActualEndDate().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getVin()
+                    && task.getVin().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getModel()
+                    && task.getModel().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getModelCode()
+                    && task.getModelCode().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getBrand()
+                    && task.getBrand().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getSurveyPoint()
+                    && task.getSurveyPoint().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getCarrier()
+                    && task.getCarrier().toLowerCase().contains(query.toLowerCase())
+                    || null != task.getDriver()
+                    && task.getDriver().toLowerCase().contains(query.toLowerCase())
+                    ) {
+                results.add(task);
+            }
+        }
+
+        return results;
     }
 }
